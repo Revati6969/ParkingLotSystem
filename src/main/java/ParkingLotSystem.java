5ae3557889a77b559c5d389be2cb619acfffce57
Revati Tekale <revitekale1910@gmail.com>
2020-04-15 15:06:22 +0530

    MODIFIED:Added if condition to check whether vehicle is already parked or not

--- src/main/java/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystem.java
@@ -1,5 +1,11 @@
 public class ParkingLotSystem {
+
+    private Object vehicle;
+
     public boolean parkVehicle(Object vehicle) {
+        if(this.vehicle != null)
+            return false;
+        this.vehicle = vehicle;
         return true;
     }
 }

b9798cac91a53e2d891b022ab7923088a9382e91
Revati Tekale <revitekale1910@gmail.com>
2020-04-15 15:05:12 +0530

    ADDED: Negative test case to park the vehicle

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,11 +1,28 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+
+    ParkingLotSystem parkingLotSystem = null;
+    Object Vehicle = null;
+
+    @Before
+    public void setUp() throws Exception {
+        Vehicle = new Object();
+        parkingLotSystem = new ParkingLotSystem();
+    }
+
     @Test
     public void givenVehicle_WhenParked_ReturnTrue() {
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
-        boolean isParked = parkingLotSystem.parkVehicle(new Object());
+        boolean isParked = parkingLotSystem.parkVehicle(Vehicle);
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenAlreadyParked_ReturnFalse() {
+        parkingLotSystem.parkVehicle(Vehicle);
+        boolean isParked = parkingLotSystem.parkVehicle(new Object());
+        Assert.assertFalse(isParked);
     }
 }

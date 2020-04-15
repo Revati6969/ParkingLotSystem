39227a3d0bb5c94770debe9dd80338787a41aed2
Revati Tekale <revitekale1910@gmail.com>
2020-04-15 20:19:11 +0530

    ADDED:Test case to notify owner after parking is full

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -2,6 +2,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ParkingLotTest {
 
     ParkingLotSystem parkingLotSystem = null;
@@ -15,31 +17,49 @@ public class ParkingLotTest {
 
     @Test
     public void givenVehicle_WhenParked_ReturnTrue() throws ParkingLotSystemException {
-        boolean isParked =  parkingLotSystem.parkVehicle(vehicle);
+        parkingLotSystem.ParkingLotSystem(1);
+        boolean isParked = parkingLotSystem.parkVehicle(vehicle);
         Assert.assertTrue(isParked);
     }
 
     @Test
-    public void givenVehicle_WhenAlreadyParked_ReturnFalse() {
+    public void givenVehicle_WhenAlreadyParked_ShouldNotBeParked() {
+        parkingLotSystem.ParkingLotSystem(2);
         try {
             parkingLotSystem.parkVehicle(vehicle);
             parkingLotSystem.parkVehicle(new Object());
         } catch (ParkingLotSystemException e) {
-            Assert.assertEquals("Parking is full", e.getMessage());
+            assertEquals("Parking is full", e.getMessage());
         }
     }
 
-   @Test
-    public void givenVehicle_WhenUnParked_ShouldReturnTrue() throws ParkingLotSystemException {
+    @Test
+    public void givenVehicle_WhenUnParked_ShouldUnpark() throws ParkingLotSystemException {
+        parkingLotSystem.ParkingLotSystem(1);
         parkingLotSystem.parkVehicle(vehicle);
-        boolean isUnParked = parkingLotSystem.unParkVehicle(vehicle);
-        Assert.assertTrue(isUnParked);
+        Object isUnParked = parkingLotSystem.unParkVehicle(vehicle);
+        Assert.assertEquals(vehicle, isUnParked);
     }
 
     @Test
     public void givenVehicle_WhenAlreadyUnParked_ShouldReturnFalse() throws ParkingLotSystemException {
-        parkingLotSystem.parkVehicle(new Object());
-        boolean isUnParked = parkingLotSystem.unParkVehicle(vehicle);
-        Assert.assertFalse(isUnParked);
+        parkingLotSystem.ParkingLotSystem(1);
+        try {
+            parkingLotSystem.parkVehicle(vehicle);
+            Object isUnParked = parkingLotSystem.unParkVehicle(new Object());
+        } catch (ParkingLotSystemException e) {
+            assertEquals("The vehicle is not parked here", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenParkingLot_WhenParkingLotIsFull_thenShouldNotifyToOwner() throws ParkingLotSystemException {
+        Owner owner = new Owner();
+        parkingLotSystem.ParkingLotSystem(2, owner);
+        Object carOne = new Object();
+        Object carTwo = new Object();
+        parkingLotSystem.parkVehicle(carOne);
+        parkingLotSystem.parkVehicle(carTwo);
+        assertEquals("Parking lot is full", owner.getMessage());
     }
 }

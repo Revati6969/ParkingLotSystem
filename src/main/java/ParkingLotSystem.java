cc8a2a61a38597705486485aa6769ed1fbafcbe5
Revati Tekale <revitekale1910@gmail.com>
2020-04-15 20:17:50 +0530

    ADDED:Constructors for the parking capacity

--- src/main/java/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystem.java
@@ -1,21 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLotSystem {
 
     private Object vehicle;
+    private int capacity;
+    private int spaceAvailable;
+    private Owner owner;
 
-    public boolean parkVehicle(Object vehicle) throws ParkingLotSystemException {
-        if(this.vehicle != null)
-          throw new ParkingLotSystemException("Parking is full");
-        this.vehicle = vehicle;
-        return true;
+    List<Object> vehiclesList = new ArrayList<>();
+
+    public void ParkingLotSystem(int capacity) {
+        this.capacity = capacity;
+        this.spaceAvailable = capacity;
+        this.owner = owner;
     }
 
-    public boolean unParkVehicle(Object vehicle) {
-        if(vehicle == null)
-            return false;
-        if(this.vehicle.equals(vehicle)) {
-            this.vehicle = null;
+    public void ParkingLotSystem(int capacity, Owner owner) {
+        this.capacity = capacity;
+        this.spaceAvailable = capacity;
+        this.owner = owner;
+    }
+
+    public boolean parkVehicle(Object vehicle) throws ParkingLotSystemException {
+        if(spaceAvailable > 0) {
+            if (this.vehicle != null) {
+                throw new ParkingLotSystemException("Parking lot is full");
+            }
+            vehiclesList.add(vehicle);
+            if (vehiclesList.size() == capacity) {
+                if (owner != null) {
+                    owner.setMessage("Parking lot is full");
+                }
+            }
+            spaceAvailable--;
             return true;
         }
-        return false;
+        throw new ParkingLotSystemException("Parking lot is full");
+    }
+
+    public Object unParkVehicle(Object vehicle) throws ParkingLotSystemException {
+        if (vehiclesList.contains(vehicle)) {
+            return vehiclesList.remove(vehiclesList.indexOf(vehicle));
+        }
+        throw new ParkingLotSystemException("The vehicle is not parked here");
     }
 }

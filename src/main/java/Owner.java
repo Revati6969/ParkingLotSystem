6b0886b1aaaf94587eedc8750f9a513cf96d5c5c
Revati Tekale <revitekale1910@gmail.com>
2020-04-15 20:15:58 +0530

    ADDED:Owner class

---
+++ src/main/java/Owner.java
@@ -0,0 +1,11 @@
+public class Owner {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

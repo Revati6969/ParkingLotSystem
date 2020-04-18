49fbc5a9446212ab1387fc0d3e047ed39edcbd18
Revati Tekale <revitekale1910@gmail.com>
2020-04-18 16:31:49 +0530

    MODIFIED: Made object of slot allotment and changed class according to that

--- src/main/java/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystem.java
@@ -1,32 +1,34 @@
+import parkinglotessetials.SlotAllotment;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingLotSystem {
 
-    private List parkedVehicles;
+    private final List parkedVehicles;
+    private SlotAllotment slotManager = null;
     private int parkingCapacity;
     private boolean parkingCapacityFull;
 
-    private final ObserversInformer observersInformer;
-
-    public ParkingLotSystem() {
-        this.parkedVehicles = new ArrayList();
-        this.observersInformer = new ObserversInformer();
+    public ParkingLotSystem(int parkingCapacity) {
+        this.parkedVehicles = new ArrayList(parkingCapacity);
+        this.slotManager = new SlotAllotment(parkingCapacity);
     }
 
     public void setParkingLotCapacity(int capacity) {
         this.parkingCapacity = capacity;
     }
+
     public boolean isThisCarPresentInTheParkingLot(Object vehicle) {
         if (this.parkedVehicles.contains(vehicle)) {
             return true;
         }
         return false;
     }
+
     public void parkTheCar(Object vehicle) throws ParkingLotSystemException {
         if (this.parkedVehicles.size() == this.parkingCapacity) {
             this.parkingCapacityFull = true;
-            this.observersInformer.informThatParkingIsFull();
             throw new ParkingLotSystemException("No space available in the parking lot!",
                     ParkingLotSystemException.ExceptionType.PARKING_FULL);
         }
@@ -34,15 +36,16 @@ public class ParkingLotSystem {
             throw new ParkingLotSystemException("Car already present in parking lot!",
                     ParkingLotSystemException.ExceptionType.CAR_ALREADY_PARKED);
         }
-        this.parkedVehicles.add(vehicle);
     }
+
     public void unParkTheCar(Object vehicle) throws ParkingLotSystemException {
-        if (this.isThisCarPresentInTheParkingLot(vehicle)) {
-            this.parkedVehicles.remove(vehicle);
-            this.observersInformer.informThatParkingIsAvailable();
-             return;
+        if (!this.isThisCarPresentInTheParkingLot(vehicle)) {
+            throw new ParkingLotSystemException("No such car present in parking lot!",
+                    ParkingLotSystemException.ExceptionType.NO_VEHICLE);
         }
-        throw new ParkingLotSystemException("No such car present in parking lot!",
-                ParkingLotSystemException.ExceptionType.NO_VEHICLE);
+        this.parkedVehicles.remove(vehicle);
+        this.slotManager.unParkUpdate(vehicle);
+        return;
     }
+
 }

995ba7940a164ef1b2759dd78468a3c42508e8c5
Revati Tekale <revitekale1910@gmail.com>
2020-04-26 23:41:39 +0530

    ADDED:Vehicle size enum class

--- src/main/java/com/bridgelabzs/Vehicle.java
+++ src/main/java/com/bridgelabzs/Vehicle.java
@@ -1,16 +1,18 @@
 package com.bridgelabzs;
 
 public class Vehicle {
+    VehicleSize vehicleSize;
     DriverType driverType;
     ParkingDateAndTime parkingDateTime;
     private String parkingDateAndTime;
     private String vehicleId;
 
-    public Vehicle(String vehicleId, DriverType driverType) {
+    public Vehicle(String vehicleId, DriverType driverType, VehicleSize vehicleSize) {
         parkingDateTime = new ParkingDateAndTime();
         this.vehicleId = vehicleId;
         parkingDateAndTime = parkingDateTime.getDateTime();
         this.driverType = driverType;
+        this.vehicleSize = vehicleSize;
     }
 
     public String getParkingDateAndTime() {
@@ -22,4 +24,6 @@ public class Vehicle {
     }
 
     public enum DriverType {HANDICAP, NORMAL}
+
+    public enum VehicleSize {LARGE, SMALL}
 }

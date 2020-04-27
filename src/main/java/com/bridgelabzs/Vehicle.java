d3467925d72111680af8ae2a76558b95a287e6fd
Revati Tekale <revitekale1910@gmail.com>
2020-04-27 19:48:11 +0530

    ADDED:Enum class for colour

--- src/main/java/com/bridgelabzs/Vehicle.java
+++ src/main/java/com/bridgelabzs/Vehicle.java
@@ -1,18 +1,20 @@
 package com.bridgelabzs;
 
 public class Vehicle {
+    VehicleColour vehicleColour;
     VehicleSize vehicleSize;
     DriverType driverType;
     ParkingDateAndTime parkingDateTime;
     private String parkingDateAndTime;
     private String vehicleId;
 
-    public Vehicle(String vehicleId, DriverType driverType, VehicleSize vehicleSize) {
+    public Vehicle(String vehicleId, DriverType driverType, VehicleSize vehicleSize, VehicleColour vehicleColour) {
         parkingDateTime = new ParkingDateAndTime();
         this.vehicleId = vehicleId;
         parkingDateAndTime = parkingDateTime.getDateTime();
         this.driverType = driverType;
         this.vehicleSize = vehicleSize;
+        this.vehicleColour =  vehicleColour;
     }
 
     public String getParkingDateAndTime() {
@@ -23,7 +25,16 @@ public class Vehicle {
         return vehicleId;
     }
 
-    public enum DriverType {HANDICAP, NORMAL}
+    public enum DriverType {
+        HANDICAP, NORMAL
+    }
 
-    public enum VehicleSize {LARGE, SMALL}
+    public enum VehicleSize {
+        LARGE, SMALL
+    }
+
+    public enum VehicleColour {
+        WHITE, BLUE
+    }
 }
+
